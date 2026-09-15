92664386c176f64fe90a5646e3d01aa7a36831da
emmanuelugwuanyi310-hub <emmanuelugwuanyi310@gmail.com>
2026-09-15 18:09:21 +0100

    feat: add screens index

---
+++ src/screens/index.js
@@ -0,0 +1,8 @@
+// Screens Index
+export { default as LoginScreen } from './LoginScreen.jsx';
+export { default as AdminLoginScreen } from './AdminLoginScreen.jsx';
+export { default as ProfileSetupScreen } from './ProfileSetupScreen.jsx';
+export { default as ProfileScreen } from './ProfileScreen.jsx';
+export { default as MarketplaceScreen } from './MarketplaceScreen.jsx';
+export { default as MessagesScreen } from './MessagesScreen.jsx';
+export { default as AdminScreen } from './AdminScreen.jsx';
